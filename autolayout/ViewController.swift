44d1be139ee07ae59ee11d922c6b41c270ca2398
Yevgeniy Kapustynskyi <ext.ykapustynskyi@vivid.money>
2020-11-11 22:25:47 +0200

    Initial Commit

---
+++ autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by AndUser on 11.11.2020.
+//  Copyright © 2020 AndUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
