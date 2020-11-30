0b167b8feb28e6b00d0f889fb7c063b2039534e0
Yevgeniy <kapust@i.ua>
2020-11-30 17:05:32 +0200

    blabla

---
+++ autolayout/ActivityView.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityView.swift
+//  autolayout
+//
+//  Created by AndUser on 30.11.2020.
+//  Copyright © 2020 AndUser. All rights reserved.
+//
+
+import Foundation
---
+++ autolayout/NewVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewVC.swift
+//  autolayout
+//
+//  Created by AndUser on 29.11.2020.
+//  Copyright © 2020 AndUser. All rights reserved.
+//
+
+import UIKit
+
+class NewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
